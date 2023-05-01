ae1e9e780482c99acd0a8cef88060e6bad04c647
Mritunjay Kumar <101097898+Mritunjay882@users.noreply.github.com>
2023-05-01 12:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
-  
+   const table = document.getElementById('myTable');
+  const rowss = prompt('Input number of rows');
+  const col = prompt('Input number of columns');
+
+	// traversing to givem value of rows
+  for (let i = 0; i < rowss; i++)
+	  {
+    const row = table.insertRow(i); // insert row will insert tr 
+		  //trvs to column
+    for (let j = 0; j < col; j++)
+			{
+	      const column = row.insertCell(j); // insert value in col
+				
+	      column.innerText = `Row-${i} Column-${j}`;
+		    }
+		}
 }
